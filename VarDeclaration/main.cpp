7d8e7e9288b5ca2410d2013132ffe991b790fd00
Georgios Karametas <geometas@hotmail.com>
2021-08-15 16:26:04 +0100

    Variable declaration

---
+++ VarDeclaration/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int salary;
+    string firstname;
+    double rate;
+
+    salary = 40000;
+    firstname = "Mary";
+    rate = .2 * .3;
+
+    int age = 23;
+    string middleName, lastName;
+
+    middleName = "Anne";
+    lastName = "Brown";
+
+    return 0;
+}
