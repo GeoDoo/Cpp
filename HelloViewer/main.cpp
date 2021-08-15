218d2017dff1022519237b248f0fca4c64546de4
Georgios Karametas <geometas@hotmail.com>
2021-08-15 16:06:53 +0100

    initial commit

---
+++ HelloViewer/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello viewer!" << endl;
+    cout << "I hope you have fun learning C++." << endl;
+    cout << endl;
+    return 0;
+}
