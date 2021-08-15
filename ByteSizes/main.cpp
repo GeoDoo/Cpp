56ce8475fb0da93abe3b1a35b4847bf433281628
Georgios Karametas <geometas@hotmail.com>
2021-08-15 16:21:01 +0100

    Data types

---
+++ ByteSizes/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << sizeof(int) << endl;
+    cout << sizeof(unsigned int) << endl;
+    cout << sizeof(double) << endl;
+    cout << sizeof(float) << endl;
+    cout << endl;
+    return 0;
+}
