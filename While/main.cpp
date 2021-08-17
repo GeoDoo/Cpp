59fdd0b7cbcee1460023a276338ca37b8c011249
Georgios Karametas <geometas@hotmail.com>
2021-08-17 17:38:09 +0100

    While

---
+++ While/main.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int count = 0;
+
+    while (count < 10)
+    {
+        cout << "Hi!" << endl;
+        ++count;
+    }
+
+    double balance, rate;
+    int years, count1;
+
+    cout << "What is the starting balance? ";
+    cin >> balance;
+    cout << "What is the annual interest rate? ";
+    cin >> rate;
+    cout << "How many years to calculate interest? ";
+    cin >> years;
+    count1 = 1;
+
+    while (count1 <= years)
+    {
+        balance *= rate;
+        cout << count1 << ": " << balance << endl;
+        ++count1;
+    }
+
+    cout << "After " << years << " years, your balance will be "
+         << balance << "." << endl;
+
+    int grade, gradeTotal, numGrades;
+    double average;
+    numGrades = gradeTotal = 0;
+
+    cout << "Enter a grade: ";
+    cin >> grade;
+
+    while (grade != -1) 
+    {
+        gradeTotal += grade;
+        ++numGrades;
+        
+        cout << "Enter a grade: ";
+        cin >> grade;
+    }
+
+    average = gradeTotal / numGrades;
+    cout << "The average grade is : " << average << endl;
+
+    return 0;
+}
