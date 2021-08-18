44cd3bec24a842b9ffa4f03d416c427d9866e414
Georgios Karametas <geometas@hotmail.com>
2021-08-18 16:46:05 +0100

    More while...

--- While/main.cpp
+++ While/main.cpp
@@ -39,17 +39,38 @@ int main()
     cout << "Enter a grade: ";
     cin >> grade;
 
-    while (grade != -1) 
+    while (grade != -1)
     {
         gradeTotal += grade;
         ++numGrades;
-        
+
         cout << "Enter a grade: ";
         cin >> grade;
     }
 
     average = gradeTotal / numGrades;
     cout << "The average grade is : " << average << endl;
+
+    double balance, rate;
+    int years, target;
+    years = 0;
+
+    cout << "What is the beginning balance? ";
+    cin >> balance;
+    cout << "What is the annual interest rate? ";
+    cin >> rate;
+    cout << "What is the target balance? ";
+    cin >> target;
+
+    while (balance < target)
+    {
+        balance *= rate;
+        ++years;
+    }
+
+    cout << "It will take " << years
+         << " years to reach the target balance "
+         << "of " << target << "." << endl;
 
     return 0;
 }
