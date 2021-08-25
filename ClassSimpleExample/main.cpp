4980b76c70ee393fab4856f14504a8cd8a8541ce
Georgios Karametas <geometas@hotmail.com>
2021-08-25 18:23:48 +0100

    Class simple example

---
+++ ClassSimpleExample/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+class Name
+{
+private:
+    string first, middle, last;
+
+public:
+    Name(string fname, string mname, string lname)
+    {
+        first = fname;
+        middle = mname;
+        last = lname;
+    }
+
+    string toString()
+    {
+        return first + " " + middle + " " + last;
+    }
+};
+
+int main()
+{
+    Name myName("Michael", "Michael", "Michael");
+
+    cout << myName.toString() << endl;
+
+    return 0;
+}
